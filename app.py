dd1b5ce118ea41dd10d0119fc8ec5bd3a5e0dd8e
risingforcebg-creator <risingforcebg@icloud.com>
2026-02-08 14:28:49 +0100

    Add no-cache headers to Flask

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, make_response
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    response = make_response(send_from_directory('.', 'index.html'))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
