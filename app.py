503e4bdafacca3cc07cdeab7a4a1bd9618d8072e
risingforcebg-creator <risingforcebg@icloud.com>
2026-02-08 14:39:54 +0100

    Remove debug endpoint

--- app.py
+++ app.py
@@ -18,14 +18,6 @@ def index():
     response.headers['Expires'] = '0'
     return response
 
-@app.route('/debug')
-def debug():
-    # Show what files exist and first 500 chars of index.html
-    files = os.listdir(BASE_DIR)
-    with open(os.path.join(BASE_DIR, 'index.html'), 'r', encoding='utf-8') as f:
-        preview = f.read(500)
-    return f"<pre>BASE_DIR: {BASE_DIR}\nFiles: {files}\n\nindex.html preview:\n{preview}</pre>"
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
